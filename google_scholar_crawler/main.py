c6ff4e70033e449f75e7c47de5c8e5d27fbd615c
BeiSheng <132643639+ReyJerry@users.noreply.github.com>
2025-05-15 21:27:27 +0800

    Update main.py

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -9,7 +9,7 @@ scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
 name = author['name']
 author['updated'] = str(datetime.now())
 author['publications'] = {v['author_pub_id']:v for v in author['publications']}
-print(json.dumps(author, indent=4))
+print(json.dumps(author, indent=2))
 os.makedirs('results', exist_ok=True)
 with open(f'results/gs_data.json', 'w') as outfile:
     json.dump(author, outfile, ensure_ascii=False)
